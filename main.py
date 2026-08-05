0c335797e925861b36f5f1b3d205ba139fa676f3
nordicmaster <master@master.com>
2026-08-05 15:24:00 +0300

    now i will run alembic locally (not in docker)

--- main.py
+++ main.py
@@ -6,25 +6,30 @@ from create_fastapi_app import create_app
 
 app = create_app(create_custom_static_urls=True)
 
-config = AuthXConfig(
-    JWT_SECRET_KEY="your-secret-key0",  # Change this!
-    JWT_TOKEN_LOCATION=["headers"],
-)
+#config = AuthXConfig(
+#    JWT_SECRET_KEY="your-secret-key0",  # Change this!
+#    JWT_TOKEN_LOCATION=["headers"],
+#)
 
-auth = AuthX(config=config)
-auth.handle_errors(app)
+#auth = AuthX(config=config)
+#auth.handle_errors(app)
 
 app.include_router(api_router)
 
 
-@app.get("/protected", dependencies=[Depends(auth.access_token_required)])
-def protected():
-    return {"message": "Protected Hello World"}
+#@app.get("/protected", dependencies=[Depends(auth.access_token_required)])
+#def protected():
+#    return {"message": "Protected Hello World"}
 
 
 @app.get("/health")
 def health_check():
     return {"status": "healthy"}
+
+
+@app.get("/t1")
+def health_check():
+    return {"e": "t1"}
 
 
 uvicorn.run(
